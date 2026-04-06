53042db6807ba44ef5d0cd7d53af9a2dab2f44ee
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-04-06 21:01:51 +0530

    add logginh to gateway with tracing

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -5,6 +5,10 @@ mod utils;
 fn main() {
     let _guard = utils::logging::init_logging();
     tracing::info!("new log");
+    tracing::debug!("debug log");
+    tracing::warn!("warn log");
+    tracing::error!("error log");
+    tracing::trace!("trace log");
     println!("hello world!")
 }
 
