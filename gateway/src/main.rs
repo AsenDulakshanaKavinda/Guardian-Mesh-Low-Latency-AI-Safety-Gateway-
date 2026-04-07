30d6756c048ba06ce5aec908dacfb986ee7980a7
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-04-07 07:36:20 +0530

    gateway config loader created

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -5,7 +5,9 @@ mod utils;
 fn main() {
     let _guard = utils::logging::init_logging();
     let settings = utils::config::AppConfig::load().expect("Failed to load configuration");
+
     tracing::info!("new log");
+    tracing::info!("the port: {}", settings.port);
     
 }
 
