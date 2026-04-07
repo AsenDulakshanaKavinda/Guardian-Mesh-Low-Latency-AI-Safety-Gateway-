8dee44eef19215479a1bd531d4fd3e8e8398477e
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-04-07 14:24:45 +0530

    upgrade gateway config loading to singolton and add the ml service config to logging

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,12 +1,11 @@
 
 mod utils;
-
-
+use crate::utils::config::APP_CONFIG;
 fn main() {
     let _guard = utils::logging::init_logging();
-    let settings = utils::config::AppConfig::load().expect("Failed to load configuration");
+    let settings = APP_CONFIG.as_ref().expect("Failed to load configuration");
 
-    tracing::info!("new log");
+    // tracing::info!("new log");
     tracing::info!("the port: {}", settings.port);
     
 }
--- ml_service/utils/__init__.py
+++ ml_service/utils/__init__.py
@@ -1,5 +1,7 @@
 from .handle_logging import get_logger
+from .handle_config import config
 
 __all__ = (
+    "config"
     "get_logger"
 )
