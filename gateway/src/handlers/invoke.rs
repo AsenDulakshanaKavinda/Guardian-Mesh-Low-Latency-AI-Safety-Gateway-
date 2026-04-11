16384e7ab1b98dd7373eaf8a02786730ad0f8670
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-04-11 13:49:52 +0530

    add the invoking handler

--- gateway/src/handlers/invoke.rs
+++ gateway/src/handlers/invoke.rs
@@ -1,7 +1,54 @@
-
+use axum::{Json, http::StatusCode, response::IntoResponse};
+use serde::Serialize;
 use mistralai_client::v1::{chat::{ChatMessage, ChatMessageRole, ChatParams}, constants::Model};
 
 use crate::services::llm_client::create_client;
+
+
+#[derive(Serialize)]
+struct InvokeResponse {
+    status: String,
+    message: String,
+    prompt: String,
+    result: String,
+}
+
+// This handler is responsible for invoking the LLM (Language Model) with a given prompt and returning the generated result.
+// It takes a string slice `prompt` as input, which represents the user's message or query to be sent to the LLM.
+// The handler calls the `invoke_llm` function, which interacts with the LLM client to generate a response based on the provided prompt.
+// If the LLM invocation is successful, it returns a JSON response with a status of "Success", a message indicating that the result was generated, 
+// the original prompt, and the generated result.
+// If the LLM invocation fails, it logs the error and returns a JSON response with a status of "Error",
+// a message indicating the failure, the original prompt, and an empty result string
+pub async fn invoking(prompt: &str) -> impl IntoResponse {
+    let result = match invoke_llm(prompt) {
+        Ok(res) => res,
+        Err(e) => {
+            tracing::error!("LLM Error: {}", e);
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(InvokeResponse {
+                    status: "Error".to_string(),
+                    message: format!("Failed to generate result: {}", e),
+                    prompt: prompt.to_string(),
+                    result: "".to_string(),
+                }),
+            ).into_response();
+        }
+    };
+
+    tracing::info!("Result successfully generated for prompt: {}", prompt);
+    (
+        StatusCode::CREATED,
+        Json(InvokeResponse {
+            status: "Success".to_string(),
+            message: "Result generated".to_string(),
+            prompt: prompt.to_string(),
+            result: result, // Move the owned String here
+        }),
+    ).into_response()
+}
+
 
 // Invokes the LLM (Language Model) to generate a response based on a user message.
 // # Arguments
@@ -15,7 +62,7 @@ use crate::services::llm_client::create_client;
 // println!("LLM Response: {}", response);
 // ```
 
-pub fn invoke_llm(prompt: &str) -> Result<String, Box<dyn std::error::Error>> {
+fn invoke_llm(prompt: &str) -> Result<String, Box<dyn std::error::Error>> {
     let new_client = create_client().map_err(|e| {
         tracing::error!("Failed to create LLM client: {}", e);
         e
