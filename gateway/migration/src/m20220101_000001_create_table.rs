f5f083073293049be8e0664d3ba176df1b5b155d
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-05-14 15:35:38 +0530

    add migratiom user tbl

---
+++ gateway/migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,45 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        // todo!();
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(UserID).string().unique_key().not_null().primary_key())
+                    .col(ColumnDef::new(User::Userame).string().not_null())
+                    .col(ColumnDef::new(User::Email).string().unique_key().not_null())
+                    .col(ColumnDef::new(User::Password).string().not_null())
+                    .col(ColumnDef::new(User::CreatedAt).date_time().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+
+        manager
+            .drop_table(Table::drop().table("post").to_owned())
+            .await
+    }
+}
+
+
+enum User {
+    Table,
+    UserID,
+    Username,
+    Email,
+    Password,
+    CreatedAt,
+}
