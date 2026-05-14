aa669f9cb689bfec66b0c77a80e0f91373f7483e
AsenDulakshanaKavinda <s22010362@ousl.lk>
2026-05-14 16:52:29 +0530

    data migration user table done

---
+++ gateway/database_migration.md
@@ -0,0 +1,6 @@
+cargo install sea-orm-cli@^2.0.0-rc
+
+sea-orm-cli migrate init
+
+
+sea-orm-cli migrate fresh
--- gateway/migration/src/m20220101_000001_create_table.rs
+++ gateway/migration/src/m20220101_000001_create_table.rs
@@ -1,7 +1,17 @@
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    UserID,
+    Username,
+    Email,
+    Password,
+    CreatedAt,
+}
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
@@ -14,8 +24,8 @@ impl MigrationTrait for Migration {
                 Table::create()
                     .table(User::Table)
                     .if_not_exists()
-                    .col(ColumnDef::new(UserID).string().unique_key().not_null().primary_key())
-                    .col(ColumnDef::new(User::Userame).string().not_null())
+                    .col(ColumnDef::new(User::UserID).string().unique_key().not_null().primary_key())
+                    .col(ColumnDef::new(User::Username).string().not_null())
                     .col(ColumnDef::new(User::Email).string().unique_key().not_null())
                     .col(ColumnDef::new(User::Password).string().not_null())
                     .col(ColumnDef::new(User::CreatedAt).date_time().not_null())
@@ -26,7 +36,7 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
+        // todo!();
 
         manager
             .drop_table(Table::drop().table("post").to_owned())
@@ -35,11 +45,3 @@ impl MigrationTrait for Migration {
 }
 
 
-enum User {
-    Table,
-    UserID,
-    Username,
-    Email,
-    Password,
-    CreatedAt,
-}
